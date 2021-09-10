2d4c34dd8bff5bd99dd71e7754f8e846f211f154
sohunjug <sohunjug@hotmail.com>
2021-09-10 11:17:06 +0800

    update

--- lua/plugins/tools/plugin.lua
+++ lua/plugins/tools/plugin.lua
@@ -2,49 +2,49 @@ local tools = {}
 local conf = require "plugins.tools.config"
 
 tools["kristijanhusak/vim-dadbod-ui"] = {
-   cmd = { "DBUIToggle", "DBUIAddConnection", "DBUI", "DBUIFindBuffer", "DBUIRenameBuffer" },
-   config = conf.vim_dadbod_ui,
-   requires = { { "tpope/vim-dadbod", opt = true } },
+    cmd = {"DBUIToggle", "DBUIAddConnection", "DBUI", "DBUIFindBuffer", "DBUIRenameBuffer"},
+    config = conf.vim_dadbod_ui,
+    requires = {{"tpope/vim-dadbod", opt = true}}
 }
 
-tools["editorconfig/editorconfig-vim"] = { ft = { "go", "typescript", "javascript", "vim", "rust", "zig", "c", "cpp" } }
+tools["editorconfig/editorconfig-vim"] = {ft = {"go", "typescript", "javascript", "vim", "rust", "zig", "c", "cpp"}}
 
-tools["glepnir/prodoc.nvim"] = { event = "BufReadPre" }
+tools["glepnir/prodoc.nvim"] = {event = "BufReadPre"}
 
 tools["brooth/far.vim"] = {
-   cmd = { "Far", "Farp" },
-   config = function()
-      vim.g["far#source"] = "rg"
-   end,
+    cmd = {"Far", "Farp"},
+    config = function()
+        vim.g["far#source"] = "rg"
+    end
 }
 
 tools["iamcco/markdown-preview.nvim"] = {
-   ft = "markdown",
-   config = function()
-      vim.g.mkdp_auto_start = 0
-   end,
+    ft = "markdown",
+    config = function()
+        vim.g.mkdp_auto_start = 0
+    end
 }
 
 tools["andymass/vim-matchup"] = {
-   opt = true,
-   setup = function()
-      require("core.utils").packer_lazy_load "vim-matchup"
-   end,
+    opt = true,
+    setup = function()
+        require("core.utils").packer_lazy_load "vim-matchup"
+    end
 }
 tools["jdhao/better-escape.vim"] = {
-   event = "InsertEnter",
-   config = function()
-      require("custom.others").better_escape()
-   end,
-   setup = function()
-      vim.g.better_escape_shortcut = "jk"
-   end,
+    event = "InsertEnter",
+    config = function()
+        require("custom.others").better_escape()
+    end,
+    setup = function()
+        vim.g.better_escape_shortcut = "jk"
+    end
 }
 
 tools["terrortylor/nvim-comment"] = {
-   cmd = "CommentToggle",
-   config = function()
-      require("custom.others").comment()
-   end,
+    cmd = "CommentToggle",
+    config = function()
+        require("custom.others").comment()
+    end
 }
 return tools
