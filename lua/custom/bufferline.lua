abb3e0cbd64936df84cd8e74e336f23fcea38624
sohunjug <sohunjug@hotmail.com>
2021-09-13 11:26:31 +0800

    update

--- lua/custom/bufferline.lua
+++ lua/custom/bufferline.lua
@@ -22,12 +22,12 @@ bufferline.setup {
       max_prefix_length = 13,
       tab_size = 20,
       show_tab_indicators = true,
-      enforce_regular_tabs = false,
+      enforce_regular_tabs = true,
       view = "multiwindow",
       show_buffer_close_icons = true,
       separator_style = "thin",
-      always_show_bufferline = true,
-      diagnostics = false, -- "or nvim_lsp"
+      always_show_bufferline = false,
+      diagnostics = "nvim_lsp",
       custom_filter = function(buf_number)
          -- Func to filter out our managed/persistent split terms
          local present_type, type = pcall(function()
