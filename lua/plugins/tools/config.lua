07ad0a30bf73fd55b85ad645941ec6dc44119e00
sohunjug <sohunjug@hotmail.com>
2021-09-15 01:51:01 +0800

    update

--- lua/plugins/tools/config.lua
+++ lua/plugins/tools/config.lua
@@ -118,21 +118,6 @@ function config.vim_dadbod_ui()
    vim.g.dbs = load_dbs()
 end
 
-function config.vim_vista()
-   vim.g["vista#renderer#enable_icon"] = 1
-   vim.g.vista_disable_statusline = 1
-   vim.g.vista_default_executive = "ctags"
-   vim.g.vista_echo_cursor_strategy = "floating_win"
-   vim.g.vista_vimwiki_executive = "markdown"
-   vim.g.vista_executive_for = {
-      vimwiki = "markdown",
-      pandoc = "markdown",
-      markdown = "toc",
-      typescript = "nvim_lsp",
-      typescriptreact = "nvim_lsp",
-   }
-end
-
 function config.zenmode()
    require "custom.zenmode"
 end
