c236991ed027443ab9fd6b69cbc759ddd586c5e2
sohunjug <sohunjug@hotmail.com>
2021-09-14 14:01:16 +0800

    scoll

--- lua/plugins/ui/config.lua
+++ lua/plugins/ui/config.lua
@@ -31,43 +31,45 @@ function config.gitsigns()
 end
 
 function config.neoscroll()
-   require("neoscroll").setup {
-      -- All these keys will be mapped to their corresponding default scrolling animation
-      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-      hide_cursor = true, -- Hide cursor while scrolling
-      stop_eof = true, -- Stop at <EOF> when scrolling downwards
-      use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-      easing_function = nil, -- Default easing function
-      pre_hook = function(info)
-         if info == "cursorline" then
-            vim.wo.cursorline = false
-         end
-      end,
-      post_hook = function(info)
-         if info == "cursorline" then
-            vim.wo.cursorline = true
-         end
-      end,
-      -- pre_hook = nil, -- Function to run before the scrolling animation starts
-      -- post_hook = nil, -- Function to run after the scrolling animation ends
-   }
+   if require("core.global").is_mac then
+      require("neoscroll").setup {
+         -- All these keys will be mapped to their corresponding default scrolling animation
+         mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+         hide_cursor = true, -- Hide cursor while scrolling
+         stop_eof = true, -- Stop at <EOF> when scrolling downwards
+         use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+         respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+         cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+         easing_function = nil, -- Default easing function
+         pre_hook = function(info)
+            if info == "cursorline" then
+               vim.wo.cursorline = false
+            end
+         end,
+         post_hook = function(info)
+            if info == "cursorline" then
+               vim.wo.cursorline = true
+            end
+         end,
+         -- pre_hook = nil, -- Function to run before the scrolling animation starts
+         -- post_hook = nil, -- Function to run after the scrolling animation ends
+      }
 
-   local t = {}
-   t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100", "sine", [['cursorline']] } }
-   t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100", "sine", [['cursorline']] } }
-   -- Use the "circular" easing function
-   t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "120", [['circular']] } }
-   t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "120", [['circular']] } }
-   -- Pass "nil" to disable the easing animation (constant scrolling speed)
-   t["<C-y>"] = { "scroll", { "-0.10", "true", "100", [['cursorlar']] } }
-   t["<C-e>"] = { "scroll", { "0.10", "false", "100", [['cursorlar']] } }
-   -- When no easing function is provided the default easing function (in this case "quadratic") will be used
-   t["zt"] = { "zt", { "140" } }
-   t["zz"] = { "zz", { "140" } }
-   t["zb"] = { "zb", { "140" } }
-   require("neoscroll.config").set_mappings(t)
+      local t = {}
+      t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100", "sine", [['cursorline']] } }
+      t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100", "sine", [['cursorline']] } }
+      -- Use the "circular" easing function
+      t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "120", [['circular']] } }
+      t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "120", [['circular']] } }
+      -- Pass "nil" to disable the easing animation (constant scrolling speed)
+      t["<C-y>"] = { "scroll", { "-0.10", "true", "100", [['cursorlar']] } }
+      t["<C-e>"] = { "scroll", { "0.10", "false", "100", [['cursorlar']] } }
+      -- When no easing function is provided the default easing function (in this case "quadratic") will be used
+      t["zt"] = { "zt", { "140" } }
+      t["zz"] = { "zz", { "140" } }
+      t["zb"] = { "zb", { "140" } }
+      require("neoscroll.config").set_mappings(t)
+   end
 end
 
 function config.indent()
