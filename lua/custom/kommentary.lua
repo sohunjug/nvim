60862e0695953d684d40882510fe951f60a8432b
sohunjug <sohunjug@hotmail.com>
2021-09-28 21:39:41 +0800

    comment

--- lua/custom/kommentary.lua
+++ lua/custom/kommentary.lua
@@ -12,6 +12,10 @@ M.config = function()
       prefer_single_line_comments = true,
       single_line_comment_string = "#",
    })
+   require("kommentary.config").configure_language("python", {
+      single_line_comment_string = "#",
+      multi_line_comment_strings = { "'''", "'''" },
+   })
 end
 
 return M
