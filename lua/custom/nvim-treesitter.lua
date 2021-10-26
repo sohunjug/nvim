ca403dcd6f0c156d118ba7cad4b040620828c36a
sohunjug <sohunjug@hotmail.com>
2021-10-26 15:25:23 +0800

    update

--- lua/custom/nvim-treesitter.lua
+++ lua/custom/nvim-treesitter.lua
@@ -14,10 +14,11 @@ M.config = function()
    vim.api.nvim_command "set foldmethod=expr"
    vim.api.nvim_command "set foldexpr=nvim_treesitter#foldexpr()"
    if vim.loop.os_uname().sysname == "Darwin" then
-      local compile = "/usr/local/bin/gcc-11"
-      if vim.loop.os_uname().machine == "arm64" then
-         compile = "/opt/homebrew/bin/gcc-11"
-      end
+      -- local compile = "/usr/local/bin/gcc-11"
+      -- if vim.loop.os_uname().machine == "arm64" then
+      -- compile = "/opt/homebrew/bin/gcc-11"
+      local compile = "/Library/Developer/CommandLineTools/usr/bin/gcc"
+      -- end
       require("nvim-treesitter.install").compilers = { compile }
    end
    require("nvim-treesitter.configs").setup {
--- lua/custom/trouble.lua
+++ lua/custom/trouble.lua
@@ -47,7 +47,7 @@ M.config = function()
          information = "",
          other = "﫠",
       },
-      use_lsp_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+      use_lsp_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
    }
 end
 
