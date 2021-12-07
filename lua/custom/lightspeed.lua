3f5f045b2899723aa23d954d9cddd29045b4ee3d
sohunjug <sohunjug@hotmail.com>
2021-12-07 14:14:50 +0800

    speed

--- lua/custom/lightspeed.lua
+++ lua/custom/lightspeed.lua
@@ -9,19 +9,22 @@ M.config = function()
       jump_on_partial_input_safety_timeout = 400,
       -- This can get _really_ slow if the window has a lot of content,
       -- turn it on only if your machine can always cope with it.
-      highlight_unique_chars = false,
+      highlight_unique_chars = true,
       grey_out_search_area = true,
       match_only_the_start_of_same_char_seqs = true,
       limit_ft_matches = 5,
-      x_mode_prefix_key = "<c-x>",
+      -- highlight_unique_chars = true,
+      -- x_mode_prefix_key = "<c-x>",
       substitute_chars = { ["\r"] = "¬" },
       instant_repeat_fwd_key = nil,
       instant_repeat_bwd_key = nil,
+      exit_after_idle_msecs = { labeled = nil, unlabeled = 1000 },
       -- If no values are given, these will be set at runtime,
       -- based on `jump_to_first_match`.
       labels = nil,
-      cycle_group_fwd_key = nil,
-      cycle_group_bwd_key = nil,
+      cycle_group_fwd_key = "<space>",
+      cycle_group_bwd_key = "<tab>",
+      repeat_ft_with_target_char = false,
    }
    vim.api.nvim_set_keymap("n", "s", "s", { noremap = true })
    vim.api.nvim_set_keymap("n", "S", "S", { noremap = true })
